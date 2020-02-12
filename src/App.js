c423e5ed176e9ca9090e4852476a3b7459917cf0
Meo Huynh <meoh92@gmail.com>
2020-02-11 18:19:39 -0800

    Add question mark functionality

--- src/App.js
+++ src/App.js
@@ -6,30 +6,41 @@ class App extends React.Component {
     super(props)
     this.state = {
       // Add magic 8 ball outcomes to the array
-      answerArray: [],
-      question: ""
+      answerArray: ['For sure', 'You wish', 'Good luck', 'I think so', 'I hope so', 'No doubt', 'Why not', 'Dream on'],
+      question: '',
+      answer: ''
     }
   }
 
   getAnswer = () => {
     // destructures answerArray out of state
-    const { answerArray } = this.state
-    // write your code here!
+    const { answerArray } = this.state;
     // Update the return statement below to reflect the outcome of your code.
-    return this.state.question
+    const randomIndex = Math.floor(Math.random() * answerArray.length);
+    return answerArray[randomIndex];
   }
 
   handleChange = (e) => {
     // gets the event from the input on change and updates state
-    this.setState({ question: e.target.value })
+    this.setState({ question: e.target.value });
   }
 
   handleSubmit = () =>{
-    // destructures question out of state
-    const { question } = this.state
-    // calls the getAnswer function and saves the outcome as answer
-    const answer = this.getAnswer()
-    this.setState({ answer: answer })
+    const { question } = this.state;
+
+    if (question.indexOf('?') !== question.length - 1 || question === '') {
+      this.setState({ answer: 'Please ask a question; don\'t forget the question mark!' })
+    } else {
+      // calls the getAnswer function and updates answer's state with it
+      this.setState({ answer: this.getAnswer() });
+    }
+  }
+
+  reset = () => {
+    this.setState({
+        question: '',
+        answer: ''
+    });
   }
 
   render(){
@@ -37,17 +48,21 @@ class App extends React.Component {
       <div>
         <h1>Magic 8 Ball</h1>
         <input
-          id="inputBox"
+          id= 'inputBox'
           type='text'
+          value={ this.state.question }
           onChange={ this.handleChange }
         />
         <br />
-        <button onClick={ this.handleSubmit }>
+        <button id="submitBtn" onClick={ this.handleSubmit }>
           Ask the Magic 8 Ball a Question
+        </button>
+        <button id="resetBtn" onClick={ this.reset }>
+          Reset
         </button>
         <p> { this.state.answer } </p>
       </div>
-    )
+    );
   }
 }
 
